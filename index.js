9cad54aaac919ee55f19273a5166bbef7d2751ba
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-10 09:24:39 -0300

    Nivelamento Javascript - Funções e Escopo

--- index.js
+++ index.js
@@ -50,3 +50,37 @@ while (contador < 3) {
     console.log("Número:", contador);
     contador++;
 }
+
+
+// Função tradicional
+function saudacao(nome) {
+    return "Olá, " + nome + "!";
+}
+console.log(saudacao("Maria"));
+
+// Arrow Function (função moderna)
+const dobro = (num) => num * 2;
+console.log(dobro(5)); // Retorna 10
+
+// Escopo de variáveis
+let global = "Sou global";
+function testarEscopo() {
+    let local = "Sou local";
+    console.log(global); // Acessa variável global
+    console.log(local); // Acessa variável local
+}
+testarEscopo();
+
+// console.log(local);  // Erro! 'local' não está acessível fora da função
+// Callback function (introdução)
+function processar(callback) {
+    console.log("Processando...");
+    callback();
+}
+processar(() => console.log("Finalizado!"));
+
+// Função assíncrona (introdução)
+async function pegarDados() {
+    return "Dados recebidos!";
+}
+pegarDados().then(console.log); // Usa .then para lidar com a resposta
